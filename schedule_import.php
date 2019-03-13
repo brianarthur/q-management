dc82cdb90377c2fc062b732f262a49c6a76f0cc1
LeonardMoen <39563194+LeonardMoen@users.noreply.github.com>
2019-03-13 13:12:22 -0400

    Added Library
    Added library which reading excel files

--- schedule_import.php
+++ schedule_import.php
@@ -1,1 +1,20 @@
-php lol
+<?php
+	// If you need to parse XLS files, include php-excel-reader
+	require('spreadsheet-reader-master/php-excel-reader/excel_reader2.php');
+
+	require('spreadsheet-reader-master/SpreadsheetReader.php');
+
+	$Reader = new SpreadsheetReader('Section01Timetable.xlsx');
+	foreach ($Reader as $Row)
+	{
+		//print_r($Row);
+		$schedule = array(
+			array(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
+			array(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
+			array(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
+			array(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
+			array(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
+		);
+		
+	}
+?>
