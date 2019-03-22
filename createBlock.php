6c0fbdd297ce66ae4a1e50b870e67d9cd5f0f47f
zhenyueyu <48294523+zhenyueyu@users.noreply.github.com>
2019-03-22 14:36:32 -0400

    Create createBlock.php

---
+++ createBlock.php
@@ -0,0 +1,2 @@
+// This will be a new feature which allows the user to create a new "Block" to use in their schedule.
+// The functionality would be that they click "Create", it prompts them to enter a title, and then they have a new block on their dash which they can use
